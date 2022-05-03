677ec875854a3c2dfd81b83c185edd23c05953a0
ZamannIsmiyev <99416682+ZamannIsmiyev@users.noreply.github.com>
2022-05-03 04:46:26 +0400

    Update gece.py

--- gece.py
+++ gece.py
@@ -13,7 +13,7 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
 from telethon.events import StopPropagation
-from config import client, USERNAME, startmesaj, qrupstart, komutlar, sahib, Kanal, Qrup
+from config import client, USERNAME, startmesaj, qrupstart, komutlar, sahib, support, group
 
 logging.basicConfig(
     level=logging.INFO,
